33a9439652ede97dd361444911cc8b138dd141b7
Nishant Agrawal <nishantagrawal2003@gmail.com>
2025-03-06 20:12:57 +0530

    Create threesum.cpp

---
+++ dsa/arrays/threesum.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());        
+}
